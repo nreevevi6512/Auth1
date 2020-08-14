79b3907774775cf20c50f8c59bee572d1ccd1704
Reeve Wayan <nreevevi6512@gmail.com>
2020-08-14 14:35:28 +0800

    REGISTER

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+//CONNECT TO DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true },
+    () => console.log("CONNECTED TO DB")
+)
+
+//IMPORT ROUTES
+const authRoute = require("./routes/auth");
+
+//ROUTE MIDDLEWARE
+app.use("/api/user", authRoute)
+
+app.listen(4000, function () {
+    console.log("SERVER IS CONNECTED:" + port)
+})
